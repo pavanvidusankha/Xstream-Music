27a553876990bb04651eadb3af1dfefb852a0354
Pavan Vidusankha <pavansamaranayake@gmail.com>
2021-08-25 14:47:45 +0530

    reformatted the code

--- src/main/java/com/example/xstream/XstreamApplication.java
+++ src/main/java/com/example/xstream/XstreamApplication.java
@@ -1,15 +1,8 @@
 package com.example.xstream;
 
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.License;
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class XstreamApplication {
@@ -18,14 +11,4 @@ public class XstreamApplication {
 		SpringApplication.run(XstreamApplication.class, args);
 	}
 
-//	@Bean
-//	public OpenAPI customOpenAPI(@Value("${application-description}") String appDesciption, @Value("${application-version}") String appVersion) {
-//		return new OpenAPI()
-//				.info(new Info()
-//						.title("Xstream Music API")
-//						.version(appVersion)
-//						.description(appDesciption)
-//						.termsOfService("http://swagger.io/terms/")
-//						.license(new License().name("Apache 2.0").url("http://springdoc.org")));
-//	}
 }
--- src/main/java/com/example/xstream/exceptions/ExceptionHelper.java
+++ src/main/java/com/example/xstream/exceptions/ExceptionHelper.java
@@ -12,6 +12,7 @@ import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MissingPathVariableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -23,38 +24,45 @@ import java.util.NoSuchElementException;
 public class ExceptionHelper extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<Object> handleNoObjectException(NoSuchElementException e){
         return new ResponseEntity<> ("No entity exists in the database ", HttpStatus.NOT_FOUND);
     }
 
     @Override
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     protected ResponseEntity<Object> handleMissingPathVariable(MissingPathVariableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         return new ResponseEntity<> ("Missing path variable ", HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(EmptyListException.class)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public ResponseEntity<Object> handleEmptyException(NoSuchElementException e){
         return new ResponseEntity<> ("No data in DB to return ", HttpStatus.NO_CONTENT);
     }
 
     @ExceptionHandler(CustomException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<String> handleBusinessLogicExceptions(CustomException e){
         return new ResponseEntity<> ("There is an issue with the  user service layer ", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @Override
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         return new ResponseEntity<> ("Request body is not supported ", HttpStatus.NOT_FOUND);
 
     }
 
     @Override
+    @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
     protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(HttpMediaTypeNotAcceptableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         return new ResponseEntity<> ("Request body is not acceptable", HttpStatus.UNSUPPORTED_MEDIA_TYPE);
 
     }
 
     @Override
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
                                                                   HttpHeaders headers, HttpStatus status, WebRequest request)
     {
@@ -68,6 +76,7 @@ public class ExceptionHelper extends ResponseEntityExceptionHandler {
 
     }
     @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
     public ResponseEntity<Object> handle(Exception ex,
                                          HttpServletRequest request, HttpServletResponse response) {
         if (ex instanceof NullPointerException) {
@@ -78,26 +87,31 @@ public class ExceptionHelper extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(NoItemFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<Object> handleNoItemException(NoItemFoundException e){
         return new ResponseEntity<> ("No item found in the database ", HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(RoleServiceException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<Object> handleNoItemException(RoleServiceException e){
         return new ResponseEntity<> ("There is an issue with role service layer", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(UserServiceException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<Object> handleNoItemException(UserServiceException e){
         return new ResponseEntity<> ("There is an issue with user auth service layer", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @Override
+    @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
     protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         return new ResponseEntity<> ("Request media type is unsupported", HttpStatus.UNSUPPORTED_MEDIA_TYPE);
     }
 
     @ExceptionHandler(IllegalStateException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<String> handleBusinessLogicExceptions(IllegalStateException e){
         return new ResponseEntity<> ("There is an issue with the provided parameters.Please check", HttpStatus.INTERNAL_SERVER_ERROR);
     }
